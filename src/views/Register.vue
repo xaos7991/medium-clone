50b9af7798cf8714f16bc3f0f6388584a3847fb4
Sergey <yougodxaso@gmail.com>
2021-01-08 16:53:48 +0300

    Изменил текст

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,7 +5,7 @@
         <div class="col-md-6 offset-md-3 col-xs-12">
           <h1 class="text-xs-center">Sing Up</h1>
           <p class="text-xs-center">
-            <router-link :to="{name: 'login'}">Need an account?</router-link>
+            <router-link :to="{name: 'login'}">Have an account?</router-link>
           </p>
           <McvValidationErrors
             v-if="validationErrors"
