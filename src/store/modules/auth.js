f0de7d84b98a51b7d2b0f45976294a937d1a2e3a
Sergey <yougodxaso@gmail.com>
2021-01-08 16:22:43 +0300

    Добавил страницу регистриации

---
+++ src/store/modules/auth.js
@@ -0,0 +1,62 @@
+import authApi from './../../../api/auth'
+import {setItem} from './../../../helpers/persistanceStorage'
+
+const state = {
+  isSubmitting: false,
+  currentUser: null,
+  validationErrors: null,
+  isLoggedIn: null
+}
+
+export const mutationTypes = {
+  registerStart: '[auth] registerStart',
+  registerSuccess: '[auth] registerSuccess',
+  registerFailure: '[auth] registerFailure'
+}
+
+export const actionTypes = {
+  register: '[auth] register'
+}
+
+const mutations = {
+  [mutationTypes.registerStart](state) {
+    state.isSubmitting = true
+    state.validationErrors = null
+  },
+  [mutationTypes.registerSuccess](state, payload) {
+    state.isSubmitting = false
+    state.currentUser = payload
+    state.isLoggedIn = true
+  },
+  [mutationTypes.registerFailure](state, payload) {
+    state.isSubmitting = false
+    state.validationErrors = payload
+  }
+}
+
+const actions = {
+  [actionTypes.register](context, credentials) {
+    return new Promise(resolve => {
+      context.commit(mutationTypes.registerStart)
+      authApi
+        .register(credentials)
+        .then(response => {
+          context.commit(mutationTypes.registerSuccess, response.data.user)
+          setItem('accessToken', response.data.user.token)
+          resolve(response.data.user)
+        })
+        .catch(result => {
+          context.commit(
+            mutationTypes.registerFailure,
+            result.response.data.errors
+          )
+        })
+    })
+  }
+}
+
+export default {
+  state,
+  mutations,
+  actions
+}
---
+++ src/views/Register.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sing Up</h1>
+          <p class="text-xs-center">
+            <router-link :to="{name: 'login'}">Need an account?</router-link>
+          </p>
+          <McvValidationErrors
+            v-if="validationErrors"
+            :validation-errors="validationErrors"
+          />
+          <form @submit.prevent="onSubmit">
+            <fieldset class="form-group">
+              <input
+                type="text"
+                class="form-control form-control-lg"
+                placeholder="Username"
+                v-model="username"
+              />
+            </fieldset>
+
+            <fieldset class="form-group">
+              <input
+                type="text"
+                class="form-control form-control-lg"
+                placeholder="Email"
+                v-model="email"
+              />
+            </fieldset>
+
+            <fieldset class="form-group">
+              <input
+                type="password"
+                class="form-control form-control-lg"
+                placeholder="Password"
+                v-model="password"
+              />
+            </fieldset>
+            <button
+              class="btn btn-lg btn-primary pull-xs-right"
+              :disabled="isSubmitting"
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import McvValidationErrors from '@/components/validationErrors'
+import {actionTypes} from '@/store/modules/auth'
+
+export default {
+  name: 'McvRegister',
+  components: {
+    McvValidationErrors
+  },
+  data() {
+    return {
+      email: '',
+      password: '',
+      username: ''
+    }
+  },
+  computed: {
+    isSubmitting() {
+      return this.$store.state.auth.isSubmitting
+    },
+    validationErrors() {
+      return this.$store.state.auth.validationErrors
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$store
+        .dispatch(actionTypes.register, {
+          email: this.email,
+          username: this.username,
+          password: this.password
+        })
+        .then(() => {
+          this.$router.push({name: 'home'})
+        })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
