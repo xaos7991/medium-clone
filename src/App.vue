b7cdb83acc6a3bfa8034cd6f9d578b03d6f6f916
Sergey <yougodxaso@gmail.com>
2021-01-08 19:03:41 +0300

    Добавил Topbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
-    Hello Medium Clone
+    <McvTopbar />
     <router-view />
   </div>
 </template>
+
+<script>
+import McvTopbar from '@/components/Topbar'
+
+export default {
+  name: 'McvApp',
+  components: {
+    McvTopbar
+  }
+}
+</script>
