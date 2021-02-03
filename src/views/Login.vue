cac16094e51e172e470e403ceeed7bc54b3e39df
Sergey <yougodxaso@gmail.com>
2021-02-03 22:13:21 +0300

    Поменял нейминг

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -45,7 +45,7 @@
 <script>
 import {mapState} from 'vuex'
 
-import McvValidationErrors from '@/components/validationErrors'
+import McvValidationErrors from '@/components/ValidationErrors'
 import {actionTypes} from '@/store/modules/auth'
 
 export default {
