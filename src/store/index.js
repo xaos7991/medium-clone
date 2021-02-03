5cad0d77a12673947e2365ccdc012c5b74512750
Sergey <yougodxaso@gmail.com>
2021-02-03 22:15:58 +0300

    Добавил новые модули

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,10 @@ import Vuex from 'vuex'
 
 import auth from '@/store/modules/auth'
 import feed from '@/store/modules/feed'
+import popularTags from '@/store/modules/popularTags'
+import article from '@/store/modules/article'
+import createArticle from '@/store/modules/createArticle'
+import editArticle from '@/store/modules/editArticle'
 
 Vue.use(Vuex)
 
@@ -12,6 +16,10 @@ export default new Vuex.Store({
   actions: {},
   modules: {
     auth,
-    feed
+    feed,
+    popularTags,
+    article,
+    createArticle,
+    editArticle
   }
 })
