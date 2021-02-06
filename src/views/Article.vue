4c9a1760e711fce5fc67ee558180838106fc22db
Sergey <yougodxaso@gmail.com>
2021-02-07 01:14:51 +0300

    Добавил кнопку подписки на статью

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -20,6 +20,15 @@
             </router-link>
             <span class="date">{{ article.createdAt }}</span>
           </div>
+          <span v-if="!isAuthor">
+            <button class="btn btn-sm btn-primary" @click="addToFavorite">
+              <i class="ion-heart" />
+              <span>
+                {{ isFavorited ? 'Unfavorite Article ' : 'Favorite Article ' }}
+                &nbsp; ({{ article.favoritesCount }})
+              </span>
+            </button>
+          </span>
           <span v-if="isAuthor">
             <router-link
               class="btn btn-outline-secondary btn-sm"
@@ -58,6 +67,7 @@
 
 <script>
 import {actionTypes as articleActionTypes} from '@/store/modules/article'
+import {actionTypes as addToFavirutesActionTypes} from '@/store/modules/addToFavorites'
 import {getterTypes as authGetterTypes} from '@/store/modules/auth'
 import {mapState, mapGetters} from 'vuex'
 import McvLoading from '@/components/Loading'
@@ -86,6 +96,14 @@ export default {
       }
 
       return this.currentUser.username === this.article.author.username
+    },
+    isFavorited: {
+      get() {
+        return this.article.favorited
+      },
+      set(value) {
+        this.article.favorited = value
+      }
     }
   },
   mounted() {
@@ -102,6 +120,18 @@ export default {
         .then(() => {
           this.$router.push({name: 'globalFeed'})
         })
+    },
+    addToFavorite() {
+      this.$store.dispatch(addToFavirutesActionTypes.addToFavorites, {
+        slug: this.$route.params.slug,
+        isFavorited: this.isFavorited
+      })
+
+      this.isFavorited
+        ? this.article.favoritesCount--
+        : this.article.favoritesCount++
+
+      this.isFavorited = !this.isFavorited
     }
   }
 }
